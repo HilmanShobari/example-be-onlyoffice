74744b10d7387036ad024e2e23805a9bf6d98b19
Hilman <hilman@loyalto.id>
2025-06-20 18:18:13 +0700

    enhance: versioning & convert to pdf

--- server.js
+++ server.js
@@ -87,8 +87,7 @@ const storage = multer.diskStorage({
         cb(null, uploadsDir);
     },
     filename: (req, file, cb) => {
-        const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-        cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+        cb(null, uuidv4() + path.extname(file.originalname));
     }
 });
 
